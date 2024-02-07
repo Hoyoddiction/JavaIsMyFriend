949a38b07807f6915cd076cde03fbc0b78e096f6
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2024-02-07 22:12:43 +0700

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,38 @@
+import javax.xml.namespace.QName;
+
+public class Car {
+    private String name;
+    private int maxSpeed;
+    private int yearOfManufacture;
+    public Car(String name, int maxSpeed, int yearOfManufacture) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+    public void name(){
+        System.out.println("name");
+    }
+    public void maxSpeed(){
+        System.out.println("maxSpeed");
+    }
+    public void yearOfManufacture(){
+        System.out.println("yearOfManufacture");
+    }
+    public static void CAR (String[] args) {
+        Car myCar = new Car("AE86",120,1985);
+        myCar.name();
+        myCar.yearOfManufacture();
+        myCar.maxSpeed();
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Max Speed: " + maxSpeed);
+        System.out.println("Year of Manufacture: " + yearOfManufacture);
+    }
+
+    public static void main (String[] args) {
+        Car myCar = new Car("AE86", 120, 1985);
+        myCar.displayInfo();
+    }
+}
