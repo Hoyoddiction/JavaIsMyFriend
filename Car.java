45a37ce372a64ce3f57e574a996e028c75877e52
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2024-02-07 23:28:43 +0700

    Add files via upload

--- Car.java
+++ Car.java
@@ -13,21 +13,21 @@ public class Car {
         System.out.println("name" + name);
     }
     public void maxSpeed(){
-        System.out.println("maxSpeed" + maxSpeed);
+        System.out.println("maxSpeed" + maxSpeed +"mph");
     }
     public void yearOfManufacture(){
-        System.out.println("yearOfManufacture" + yearOfmanufacture);
+        System.out.println("yearOfManufacture" + yearOfManufacture);
     }
 
 
     public void displayInfo() {
         System.out.println("Name: " + name);
-        System.out.println("Max Speed: " + maxSpeed);
+        System.out.println("Max Speed: " + maxSpeed + "mph");
         System.out.println("Year of Manufacture: " + yearOfManufacture);
     }
 
     public static void main (String[] args) {
-        Car myCar = new Car("AE86", 120, 1985);
+        Car myCar = new Car("AE86", 125, 1985);
         myCar.displayInfo();
     }
 }
